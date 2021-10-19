43939b6d16081d7d8eaa5b827900b4df45159fb5
ckpss88109 <92736292+ckpss88109@users.noreply.github.com>
2021-10-19 21:43:56 +0800

    Update playlist6.js

--- scripts/playlist6.js
+++ scripts/playlist6.js
@@ -5,8 +5,103 @@
 		src:"抖音精選/yt1s.com - Jason 錯過最遺憾不是離開你是還沒說出喜歡你動態歌詞Lyrics.mp3"
 	},
 	{
+		name:"那女孩對我說",
+		artist:"Uu",
+		src:"抖音精選/yt1s.com - Uu 那女孩對我說原唱黃義達遍體鱗傷的我一天也沒再愛過動態歌詞Lyrics.mp3"
+	},
+	{
+		name:"疑心病",
+		artist:"任然",
+		src:"抖音精選/yt1s.com - 任然 疑心病我會用這一生為你守候動態歌詞Lyrics.mp3"
+	},
+	{
+		name:"平凡之路",
+		artist:"NA",
+		src:"抖音精選/yt1s.com - 平凡之路.mp3"
+	},
+	{
+		name:"側臉",
+		artist:"于果",
+		src:"抖音精選/yt1s.com - 抖音側臉于果動態歌詞Lyrics.mp3"
+	},
+	{
+		name:"追光者",
+		artist:"汪蘇瀧",
+		src:"抖音精選/yt1s.com - 汪蘇瀧 追光者高清去雜音動態歌詞Lyrics.mp3"
+	}, 
+	{
+		name:"像魚",
+		artist:"王貳浪",
+		src:"抖音精選/yt1s.com - 王貳浪 像魚我要記住你的樣子像魚記住水的擁抱動態歌詞MV MCMC.mp3"
+	}, 
+	{
+		name:"往後餘生COVER",
+		artist:"王貳浪",
+		src:"抖音精選/yt1s.com - 王貳浪 往後餘生COVER 馬良心底溫柔是你目光所致也是你動態歌詞Lyrics.mp3"
+	},
+	{
+		name:"把回憶拼好給你",
+		artist:"王貳浪",
+		src:"抖音精選/yt1s.com - 王貳浪 把回憶拼好給你動態歌詞Lyrics.mp3"
+	}, 
+	{
+		name:"錯位時空",
+		artist:"艾辰",
+		src:"抖音精選/yt1s.com - 艾辰 錯位時空我吹過你吹過的晚風那我們算不算相擁動態歌詞Lyrics.mp3"
+	},
+	{
+		name:"一直想著他",
+		artist:"莊心妍",
+		src:"抖音精選/yt1s.com - 莊心妍 一直想著他他會想著我嗎動態歌詞Lyrics.mp3"
+	}, 
+	{
+		name:"再見只是陌生人",
+		artist:"莊心妍",
+		src:"抖音精選/yt1s.com - 莊心妍 再見只是陌生人也許我錯過幸福的時分動態歌詞Lyrics.mp3"
+	}, 
+	{
+		name:"終於失去你",
+		artist:"莊心妍",
+		src:"抖音精選/yt1s.com - 莊心妍 好可惜終於失去你動態歌詞Lyrics.mp3"
+	}, 
+	{
+		name:"起風了",
+		artist:"買辣椒也用券",
+		src:"抖音精選/yt1s.com - 買辣椒也用券 起風了我曾難自拔於世界之大也沉溺於其中夢話動態歌詞Lyrics.mp3"
+	}, 
+	{
+		name:"你的答案",
+		artist:"阿冗",
+		src:"抖音精選/yt1s.com - 阿冗 你的答案也許我只能沉默眼淚濕潤眼眶可又不甘懦弱動態歌詞Lyrics.mp3"
+	}, 
+	{
+		name:"煙幕Smokescreen",
+		artist:"陳忻玥 Vicky Chen",
+		src:"抖音精選/yt1s.com - 陳忻玥 Vicky Chen 煙幕 Smokescreen 彩虹六部曲第一次La Boum Official MV KKTV原創電視劇.mp3"
+	},
+	{
+		name:"你的酒館對我打了烊",
+		artist:"陳雪凝",
+		src:"抖音精選/yt1s.com - 陳雪凝 你的酒館對我打了烊動態歌詞Lyrics.mp3.webm"
+	},
+	{
+		name:"綠色",
+		artist:"陳雪凝",
+		src:"抖音精選/yt1s.com - 陳雪凝 綠色說不痛苦那是假的畢竟我的心也是肉做的動態歌詞Lyrics.mp3"
+	}, 
+	{
+		name:"BINGBIAN病變",
+		artist:"鞠文嫻",
+		src:"抖音精選/鞠文嫻 - BINGBIAN病變 (女聲版) Feat. Deepain【動態歌詞Lyrics】.mp3"
+	}, 
+	{
+		name:"晚安",
+		artist:"顏人中",
+		src:"抖音精選/顏人中 晚安迂回一句晚安多情人卻自找難堪中文動態歌詞Lycris完整版.mp3"
+	}, 
+	{
 		name:"夜空中最亮的星",
 		artist:"Rown",
 		src:"抖音精選/yt1s.com - Rown 夜空中最亮的星 從未聽過那麼好聽的聲音 高清版 Karendaidai.mp3"
-	},
+	}
 ];
