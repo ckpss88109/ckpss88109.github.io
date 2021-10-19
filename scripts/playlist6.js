8e34bbc96e79ee826e11f1cab07f3dec1f1ca901
ckpss88109 <92736292+ckpss88109@users.noreply.github.com>
2021-10-19 21:30:23 +0800

    Update playlist6.js

--- scripts/playlist6.js
+++ scripts/playlist6.js
@@ -1,13 +1,13 @@
 ﻿trackList = [
 	{
-		name:" Jason-錯過",
+		name:"錯過",
 		artist:"Jason",
-		src:"抖音精選/yt1s.com - Jason  錯過最遺憾不是離開你是還沒說出喜歡你動態歌詞Lyrics.mp3"
+		src:"抖音精選/yt1s.com - Jason 錯過最遺憾不是離開你是還沒說出喜歡你動態歌詞Lyrics.mp3"
 	},
 	{
 		name:"夜空中最亮的星",
 		artist:"Rown",
-		src:"抖音精選/yt1s.com - Rown  夜空中最亮的星 從未聽過那麼好聽的聲音 高清版 Karendaidai"
-	},
+		src:"抖音精選/yt1s.com - Rown 夜空中最亮的星 從未聽過那麼好聽的聲音 高清版 Karendaidai.mp3"
+	}
 
 ];
